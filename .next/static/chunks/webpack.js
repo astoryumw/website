8f3f568c43d5fd84ea7dfcd51c66f35257f3f192
offthecookies <aws14a@acu.edu>
2021-05-01 19:53:44 -0400

    Fixed issue with site not running on safari

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c576a9bed3b51d68114e";
+/******/ 	var hotCurrentHash = "b964ca1f3198ff25f7ed";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
